2ac47dc30070abcaf679a948ed4d8808d50f42f3
Dvir <dvir@simplee.com>
2017-02-14 15:47:05 +0200

    Add post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,23 @@
 Rails.application.routes.draw do
+  # get 'posts/index'
+
+  # get 'posts/show'
+
+  # get 'posts/new'
+
+  # get 'posts/edit'
+
+  resources :posts
+
   get 'welcome/contact'
 
-  get 'welcome/index'
+  # get 'welcome/index'
 
-  get 'welcome/about'
+  # get 'welcome/about'
+
+  get 'about' => 'welcome#about'
+
+  get 'contact' => 'welcome#contact'
 
   root to: 'welcome#index'
 
