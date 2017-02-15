3b4f68b358dfccda4b0d0ab6dadb435a54b0179e
Dvir <dvir@simplee.com>
2017-02-15 17:23:06 +0200

    fix problem of sending confirmation mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.development? || Rails.env.production?
+if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
@@ -6,6 +6,18 @@ if Rails.env.development? || Rails.env.production?
     authentication: :plain,
     user_name:      ENV['SENDGRID_USERNAME'],
     password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
+
+elsif Rails.env.development?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    password:      'vwm91e284668',
+    user_name:       'app63628433@heroku.com',
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
