0e8e2958c312350827821f260430d5a440636f52
Dvir <dvir@simplee.com>
2017-02-16 17:06:31 +0200

    adding delete option for topics, posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,20 @@ class PostsController < ApplicationController
     authorize @post
   end
 
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    title = @post.title
+    authorize @post
+
+    if @post.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfuly."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting this post."
+      render :show
+    end
+  end
 
   private 
   def post_params
