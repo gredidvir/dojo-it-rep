dec630942a835e5be712034abc2327a3a385c72b
Dvir <dvir@simplee.com>
2017-02-16 11:11:59 +0200

    adding image to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216073956) do
+ActiveRecord::Schema.define(version: 20170216090050) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170216073956) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "post_image"
     t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
